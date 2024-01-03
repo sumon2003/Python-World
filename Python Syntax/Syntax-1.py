e303ba236b951997828145bdab3124d6061c1f72
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-03 18:00:34 +0600

    Python Syntax

---
+++ Python Syntax/Syntax-1.py
@@ -0,0 +1,10 @@
+if 5 > 2:
+    print("Five is greater than two!")
+    
+"""
+Syntax Error: Python will give you an error if you skip the indentation
+
+if 5 > 2:
+print("Five is greater than two!") 
+
+"""    
---
+++ Python Syntax/Syntax-2.py
@@ -0,0 +1,14 @@
+if 5 > 2:
+    print("5 is greater than 2!")
+if 5 > 2:
+        print("5 is greater than 2!")
+
+"""
+Syntax Error: You have to use the same number of spaces in the same block of code.
+              Otherwise Python will give you an error.
+
+if 5 > 2: 
+    print("5 is greater than 2!")    
+       print("5 is greater than 2!")
+       
+"""            
