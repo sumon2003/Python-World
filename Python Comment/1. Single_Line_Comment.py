05eae774983159049e2bc3b23f33fef34f979dcc
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-03 18:05:37 +0600

    Python Comment

---
+++ Python Comment/1. Single_Line_Comment.py
@@ -0,0 +1,16 @@
+"""   بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   """
+ #==->  Md Sumon Hossain Khan <-==#   
+
+
+#Comment::==---> Start with a "#"
+
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code.
+
+#This is a comment
+print("Hello, World!") #This is a comment
+
+#print("Hello, SUMON!")
+print("Hello, SUMON!")
+
