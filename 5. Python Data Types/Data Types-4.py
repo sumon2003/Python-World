8003f713476124449f7225f9f08493bbd4ce2501
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-04 11:06:18 +0600

    Python Data Types

---
+++ 5. Python Data Types/Data Types-4.py
@@ -0,0 +1,66 @@
+"""   بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   """
+ #==->  Md Sumon Hossain Khan <-==#   
+
+"""
+Setting the Data Type::>
+In Python, the data type is set when you assign a value to a variable.
+
+"""
+
+#11. Data Type 'bool':
+x = True
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#=================================================
+
+#12. Data Types 'bytes':
+x = b"Hello"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#=================================================
+
+#13. Data Type 'bytearray':
+x = bytearray(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#=================================================
+
+#14. Data Type 'memoryview':
+x = memoryview(bytes(5))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#=================================================
+
+#15. Data Type 'NoneType':
+x = None
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+
+
+
