e70658f04515c94a7579729b8e07e572d5840d4e
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-04 11:05:05 +0600

    Python Comment

---
+++ 3. Python Comment/2. Multi_Line_Comment.py
@@ -0,0 +1,20 @@
+"""   بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   """
+ #==->  Md Sumon Hossain Khan <-==#  
+
+print("Hello, World!")
+"""
+Multiline Comment::==->
+
+1. Python does not really have a syntax for multiline comments.
+2. To add a multiline comment you could insert a # for each line.
+   Example:   #This is a single line comment
+              #This is a single line comment
+              #This is a single line comment   
+3. Or, not quite as intended, you can use a multiline string.
+   Since Python will ignore string literals that are not assigned to a variable, 
+   you can add a multiline string (triple quotes) in your code,
+   and place your comment inside it.
+   Example: This whole script is also a multiline comment.
+
+"""
+print("Hello, Sumon!")
